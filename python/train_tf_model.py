c5ff26953ca02cf5d03cd9360e3f15346cf941bf
Nicolas Burrus <nb@occipital.com>
2019-02-11 23:06:02 +0100

    train_tf_model.py: connect together the pieces to do some actual training.

--- python/train_tf_model.py
+++ python/train_tf_model.py
@@ -10,6 +10,7 @@ import numpy as np
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
+layers = keras.layers
 
 from common import *
 
@@ -21,17 +22,19 @@ def parse_command_line():
 
 def decode_image(image_bytes):
     image = tf.image.decode_png(image_bytes)
+    image = tf.cast(image, tf.float32)
     image = tf.multiply(image, 1.0 / 255.)
     return image
 
 def patch_example_parser(record):
     keys_to_features = {
         "image/is_background": tf.FixedLenFeature((), tf.int64, default_value=None),
-        "image/rgb_color": tf.FixedLenFeature((), tf.float64, default_value=None),
+        "image/rgb_color": tf.FixedLenFeature([3], tf.float32, default_value=None),
         "image/encoded": tf.FixedLenFeature((), tf.string, default_value=None),
         'image/height': tf.FixedLenFeature((), tf.int64, default_value=None),
         'image/width': tf.FixedLenFeature((), tf.int64, default_value=None),
     }
+
     parsed = tf.parse_single_example(record, keys_to_features)
     
     # Perform additional preprocessing on the parsed data.
@@ -40,9 +43,9 @@ def patch_example_parser(record):
     is_background = tf.cast(parsed["image/is_background"], tf.int32)
     rgb_color = parsed["image/rgb_color"]
     
-    features = {"image": image}
+    # features = {"image": image}
     
-    return features, rgb_color
+    return image, rgb_color
 
 if __name__ == "__main__":
     args = parse_command_line()
@@ -52,6 +55,26 @@ if __name__ == "__main__":
     
     train_dataset = tf.data.TFRecordDataset(train_files)
     train_dataset = train_dataset.map(lambda r: patch_example_parser(r))
-    train_dataset = train_dataset.shuffle(100)
+    train_dataset = train_dataset.shuffle(10000)
     train_dataset = train_dataset.batch(20)
     train_dataset = train_dataset.repeat()
+
+    with tf.Session() as sess:
+        it = train_dataset.make_one_shot_iterator()
+        print (sess.run(it.get_next()))
+
+    model = tf.keras.Sequential()
+    # Adds a densely-connected layer with 64 units to the model:
+    model.add(layers.Flatten(name='flatten', input_shape=(patch_size,patch_size,3)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(64, activation='relu'))
+    # Add a softmax layer with 3 output units:
+    model.add(layers.Dense(3, activation='relu', name='final'))
+
+    model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+              loss='mse',
+              metrics=['accuracy'])
+
+    cb = tf.keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None)
+
+    model.fit(train_dataset, epochs=20, steps_per_epoch=100, callbacks=[cb])
