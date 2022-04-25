1c3b4dc45ee2cd7429c9871e3f12dc6bc8e0b0f7
Nicolas Burrus <nicolas@burrus.name>
2022-04-25 12:49:52 +0200

    New utility script to generate aliased images from GT labels.

---
+++ charts/inputs/generate_plots/generate_aliased_from_labels.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import argparse, json, os, random, sys
+from pathlib import Path
+
+from dlcharts.common.dataset import LabeledImage
+
+import cv2
+from dlcharts.common.utils import swap_rb
+
+from zv.log import zvlog
+
+parser = argparse.ArgumentParser(description='Generate aliased rgb images from GT labels.')
+parser.add_argument('json_files', type=Path, help='Json files with the labels', nargs='+')
+args = parser.parse_args()
+
+for json_file in args.json_files:
+    im = LabeledImage(json_file)
+    im.compute_labels_as_rgb ()
+    cv2.imwrite(str(im.json_file.with_suffix('.aliased.png')), swap_rb(im.labels_as_rgb))
